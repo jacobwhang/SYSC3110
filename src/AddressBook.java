0dd81724cdff35dddbd51299f78c0c476c5edfad
gottijoy2 <jacobjwhang@Gmail.com>
2024-09-16 11:15:32 -0400

    second change

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,18 +1,20 @@
 import java.util.LinkedList;
 public class AddressBook {
     LinkedList<BuddyInfo> collection = new LinkedList<>();
-    BuddyInfo buddy1 = new BuddyInfo();
-    BuddyInfo buddy2 = new BuddyInfo();
-    BuddyInfo buddy3 = new BuddyInfo();
 
-    private void addBuddy(BuddyInfo buddy){
+    public void addBuddy(BuddyInfo buddy){
         collection.add(buddy);
     }
 
-    private void removeBuddy(BuddyInfo buddy){
+    public void removeBuddy(BuddyInfo buddy){
         collection.remove(buddy);
     }
     public static void main(String[] args) {
         System.out.println("Address Book");
+        AddressBook book = new AddressBook();
+        BuddyInfo buddy1 = new BuddyInfo("Tom", "Carleton", 613);
+        book.addBuddy(buddy1);
+        book.removeBuddy(buddy1);
+
     }
 }
